dfc72078a107437de47e79bb1ca00892164b2fdf
Irfan Baig <riverfr0zen@elitemail.org>
2022-02-01 12:51:01 -0500

    moved shifty circle to its own module

--- explorations/src/explore_bevy_examples/lyon_egs.rs
+++ explorations/src/explore_bevy_examples/lyon_egs.rs
@@ -4,7 +4,7 @@ use bevy::core::FixedTimestep;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
 pub mod eglib;
-
+pub mod shiftyc;
 
 pub fn lyon_eg_app() {
     App::new()
@@ -32,23 +32,23 @@ pub fn shifty_circle_app() {
     App::new()
         .insert_resource(WindowDescriptor {
             title: "Shifty Circle".to_string(),
-            width: eglib::WINDOW_WIDTH,
-            height: eglib::WINDOW_HEIGHT,
+            width: shiftyc::WINDOW_WIDTH,
+            height: shiftyc::WINDOW_HEIGHT,
             ..Default::default()
         })
-        .insert_resource(ClearColor(eglib::CLEAR_COLOR))
+        .insert_resource(ClearColor(shiftyc::CLEAR_COLOR))
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
-        .add_startup_system(eglib::setup_shifty_circle)
+        .add_startup_system(shiftyc::setup_shifty_circle)
         // Note setting with_run_criteria on a single system
         // (Found it here: https://bevy-cheatbook.github.io/programming/run-criteria.html#run-criteria-labels)
         .add_system(
-            eglib::translate_circle
-                .with_run_criteria(FixedTimestep::step(eglib::SHIFTY_CIRCLE_STEP)))
+            shiftyc::translate_circle
+                .with_run_criteria(FixedTimestep::step(shiftyc::SHIFTY_CIRCLE_STEP)))
         .add_system(
-            eglib::change_circle_destination
-                .with_run_criteria(FixedTimestep::step(eglib::SHIFTY_CHANGE_STEP)))
+            shiftyc::change_circle_destination
+                .with_run_criteria(FixedTimestep::step(shiftyc::SHIFTY_CHANGE_STEP)))
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .run();
