94aa7717658cd92987da54ad8f4b1319295e795b
Irfan Baig <riverfr0zen@elitemail.org>
2022-01-31 22:37:57 -0500

    shifty: i think the shiftiness goal has been achieved

--- explorations/src/explore_bevy_examples/lyon_egs/eglib.rs
+++ explorations/src/explore_bevy_examples/lyon_egs/eglib.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
-use bevy_prototype_lyon::shapes::Circle;
 use rand::Rng;
 use rand::prelude::thread_rng;
 
@@ -50,25 +49,33 @@ pub fn path_eg_setup(mut commands: Commands) {
 
 pub const SHIFTY_CIRCLE_RADIUS: f32 = 100.0;
 pub const SHIFTY_CIRCLE_STROKE: f32 = 1.0;
-pub const SHIFTY_CIRCLE_SPEED: f32 = 1.0; // I.e. how much it translates per step
+pub const SHIFTY_CIRCLE_SPEED: f32 = 50.0; // I.e. how much it translates per step
 pub const SHIFTY_CIRCLE_STEP: f64 = 0.01;
-pub const SHIFTY_CHANGE_STEP: f64 = 0.1;
+pub const SHIFTY_CHANGE_STEP: f64 = 0.5;
+pub const WINDOW_WIDTH: f32 = 2400.0;
 pub const WINDOW_HEIGHT: f32 = 1600.0;
-pub const WINDOW_WIDTH: f32 = 1600.0;
 
 
 #[derive(Component)]
 pub struct ShiftyCircle;
 
 
+// #[derive(Component, Clone, Copy, PartialEq, Eq)]
+#[derive(Component)]
+pub struct Destination {
+    x: f32,
+    y: f32,
+}
+
+
 pub fn setup_shifty_circle(mut commands: Commands) {
-    let mut rng = thread_rng();
+    // let mut rng = thread_rng();
     let mycircle = shapes::Circle {
         radius: SHIFTY_CIRCLE_RADIUS,
         // I thought this could be a way to start the circle in a random
         // position, but found out what this does is set the center of
         // the circle **in the world**. Any future translations would
-        // be based off this random center. Of, course that isn't what
+        // be based off this center. Of, course that isn't what
         // I wanted. (What I wanted is in fact actually achieved in the 
         // first cycle of the`change_circle_destination` system).
         //
@@ -88,35 +95,44 @@ pub fn setup_shifty_circle(mut commands: Commands) {
         },
         Transform::default(),
     ))
-    .insert(ShiftyCircle);
+    .insert(ShiftyCircle)
+    .insert(Destination { x: 0.0, y: 0.0 });
 }
 
 
-// From snake:
-// pub fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
-pub fn translate_circle(mut q: Query<&mut Transform, With<ShiftyCircle>>) {
-    for mut transform in q.iter_mut() {
-        // println!("{}", transform.translation.x);
-        transform.translation.x += SHIFTY_CIRCLE_SPEED;
-        transform.translation.y += SHIFTY_CIRCLE_SPEED / 2.0;
+pub fn translate_circle(mut q: Query<(&mut Transform, &Destination)>) {
+    for (mut transform, dest) in q.iter_mut() {
+        if dest.x > transform.translation.x {
+            transform.translation.x += SHIFTY_CIRCLE_SPEED;
+        }
+        if dest.x < transform.translation.x {
+            transform.translation.x -= SHIFTY_CIRCLE_SPEED;
+        }
+
+        if dest.y > transform.translation.y {
+            transform.translation.y += SHIFTY_CIRCLE_SPEED;
+        }
+        if dest.y < transform.translation.y {
+            transform.translation.y -= SHIFTY_CIRCLE_SPEED;
+        }
     }
 
 }
 
 
-pub fn change_circle_destination(mut q: Query<&mut Transform, With<ShiftyCircle>>) {
+pub fn change_circle_destination(mut q: Query<&mut Destination, With<ShiftyCircle>>) {
 
     let mut rng = thread_rng();
-    for mut transform in q.iter_mut() {
-        transform.translation.x = rng.gen_range(
+    for mut dest in q.iter_mut() {
+        dest.x = rng.gen_range(
             -WINDOW_WIDTH/2.0+SHIFTY_CIRCLE_RADIUS..WINDOW_WIDTH/2.0-SHIFTY_CIRCLE_RADIUS
         );
-        transform.translation.y = rng.gen_range(
+        dest.y = rng.gen_range(
             -WINDOW_HEIGHT/2.0+SHIFTY_CIRCLE_RADIUS..WINDOW_HEIGHT/2.0-SHIFTY_CIRCLE_RADIUS
         );
-        println!("x: {}", transform.translation.x);
-        println!("y: {}", transform.translation.y);
-        println!("---");
+        // println!("x: {}", dest.x);
+        // println!("y: {}", dest.y);
+        // println!("---");
     }
 
 }
